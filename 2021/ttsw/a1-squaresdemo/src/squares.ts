0a4278152e86eb388f1f9d3fbb3d63c7b74c8633
Viswanatha Swamy <vishipayyallore@yahoo.com>
2021-10-12 06:58:38 +0530

    Today's work

--- 2021/ttsw/a1-squaresdemo/src/squares.ts
+++ 2021/ttsw/a1-squaresdemo/src/squares.ts
@@ -1,11 +1,13 @@
 
 function squares(values: number[]): number[] {
+
     const results = values.map(x => x * x);
 
     return results;
 }
 
 const squaresv1 = (values: number[]): number[] => {
+
     return values.map(x => x * x);
 }
 
